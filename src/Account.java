2fe1beb142a26ddaedc2aa625f751f49a281c95d
Joachim Olsson <joaco@kth.se>
2023-04-26 16:12:29 +0200

    create fields, constructors and base methods in account and bank class, add more

--- src/Account.java
+++ src/Account.java
@@ -1,5 +1,75 @@
 package src;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Account class
+ * 
+ * This class represents a bank accounts within the bank.
+ * 
+ * @author Joachim Olsson
+ * @author Alexander Järvheden
+ * @author Aziz Ali
+ * @version 2023-04-26
+ * 
+ */
+
 public class Account {
+
+    private String accountNumber;
+    private String accountType;
+    private String accountOwner;
+    private double balance;
+    private double interestRate;
+    private List<String> transactionHistory;
+
+    public Account(String accountNumber, String accountType, String accountOwner, double interestRate) {
+        this.accountNumber = accountNumber;
+        this.accountType = accountType;
+        this.accountOwner = accountOwner;
+        this.interestRate = interestRate;
+        this.transactionHistory = new ArrayList<>();
+        this.balance = 0;
+    }
+
+    public void deposit(double amount) {
+        this.balance += amount;
+        transactionHistory.add("Deposit: " + amount);
+    }
+
+    public boolean withdraw(double amount) {
+        if (amount <= this.balance) {
+            this.balance -= amount;
+            transactionHistory.add("Withdraw: " + amount);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean transfer(Account destination, double amount) {
+        if (withdraw(amount)) {
+            destination.deposit(amount);
+            transactionHistory.add("Transfer to " + destination.getAccountNumber() + ": " + amount);
+            return true;
+        }
+        return false;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+
+    public List<String> getTransactionHistory() {
+        return this.transactionHistory;
+    }
+
+    public double calculateInterest() {
+        return this.balance * this.interestRate;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
 
 }
--- src/Bank.java
+++ src/Bank.java
@@ -1,5 +1,100 @@
 package src;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Bank class
+ * 
+ * @author Joachim Olsson
+ * @author Alexander Järvheden
+ * @author Aziz Ali
+ * @version 2023-04-26
+ * 
+ */
+
 public class Bank {
+
+    private String clearingNumber;
+    private double totalCapitalInBank;
+    private double liquidCash;
+    private double totalCapitalLoanedOut;
+    private Map<String, Account> accounts;
+
+    public Bank(String clearingNumber) {
+        this.clearingNumber = clearingNumber;
+        this.totalCapitalInBank = 0;
+        this.liquidCash = 0;
+        this.totalCapitalLoanedOut = 0;
+        this.accounts = new HashMap<String, Account>();
+    }
+
+    public String getClearingNumber() {
+        return this.clearingNumber;
+    }
+
+    public double getTotalCapitalInBank() {
+        return this.totalCapitalInBank;
+    }
+
+    public double getLiquidCash() {
+        return this.liquidCash;
+    }
+
+    public double getTotalCapitalLoanedOut() {
+        return this.totalCapitalLoanedOut;
+    }
+
+    public Map<String, Account> getAccounts() {
+        return this.accounts;
+    }
+
+    public void setClearingNumber(String clearingNumber) {
+        this.clearingNumber = clearingNumber;
+    }
+
+    public void setTotalCapitalInBank(double totalCapitalInBank) {
+        this.totalCapitalInBank = totalCapitalInBank;
+    }
+
+    public void setLiquidCash(double liquidCash) {
+        this.liquidCash = liquidCash;
+    }
+
+    public void setTotalCapitalLoanedOut(double totalCapitalLoanedOut) {
+        this.totalCapitalLoanedOut = totalCapitalLoanedOut;
+    }
+
+    public void addAccount(String accountNumber, Account account) {
+        this.accounts.put(accountNumber, account);
+    }
+
+    public void removeAccount(String accountNumber) {
+        this.accounts.remove(accountNumber);
+    }
+
+    public void addCapital(double amount) {
+        this.totalCapitalInBank += amount;
+    }
+
+    public void removeCapital(double amount) {
+        this.totalCapitalInBank -= amount;
+    }
+
+    public void addLiquidCash(double amount) {
+        this.liquidCash += amount;
+    }
+
+    public void removeLiquidCash(double amount) {
+        this.liquidCash -= amount;
+    }
+
+    public void addCapitalLoanedOut(double amount) {
+        this.totalCapitalLoanedOut += amount;
+    }
+
+    public void removeCapitalLoanedOut(double amount) {
+        this.totalCapitalLoanedOut -= amount;
+    }
 
 }
