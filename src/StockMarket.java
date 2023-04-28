9a1a9d51ad47ca4f935bd45c9b09743d9e5e465c
AlexanderJarvheden <alexander.jarvheden@gmail.com>
2023-04-28 21:05:15 +0200

    Try make a form

--- src/StockMarket.java
+++ src/StockMarket.java
@@ -1,5 +1,36 @@
-package src;
+// package src;
 
-public class StockMarket {
+// public class StockMarket {
 
-}/*New class */
+// }/*New class */
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class BankApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Bank Application");
+
+        Button createAccountBtn = new Button("Create Account");
+        Button depositBtn = new Button("Deposit Money");
+        Button withdrawBtn = new Button("Withdraw Money");
+        Button balanceBtn = new Button("Check Balance");
+        Button exitBtn = new Button("Exit");
+
+        StackPane layout = new StackPane();
+        layout.getChildren().addAll(createAccountBtn, depositBtn, withdrawBtn, balanceBtn, exitBtn);
+
+        Scene scene = new Scene(layout, 300, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
