a29f6b1db329b868cd45991e412749081e06ec29
AlexanderJarvheden <alexander.jarvheden@gmail.com>
2023-05-03 18:26:51 +0200

    Update onPress function for the sign in button

--- Interface/app/index.js
+++ Interface/app/index.js
@@ -2,21 +2,34 @@ import { ScrollView, TouchableOpacity, View } from "react-native";
 import { Link, Stack } from "expo-router";
 import SignIn from "../components/signin/signIn";
 import { COLORS, icons, images, SIZES } from '../constants';
+import StockMarketTab from "../components/stockmarket/StockMarket";
+import React, { useState } from 'react';
 
 export default function Home() {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
+  const handleSignIn = () => {
+    // Your sign-in logic here
+
+    // Set the sign-in status to true if the sign-in is successful
+    setIsSignedIn(true);
+  };
+
   return (
-    <View style={{ flex: 1, backgroundColor: COLORS.primary, justifyContent: "center", alignItems: "center" }}>
-      {/* Use the `Screen` component to configure the layout. */}
-      <ScrollView>
-        <TouchableOpacity>
-          <Stack.Screen options={{ title: "Welcome to your bank", headerStyle: {backgroundColor: COLORS.red}, headerTitleStyle: { fontSize: 30, color: COLORS.white }}} />
-          {/* Use the `Link` component to enable optimized client-side routing. */}
-          <SignIn/>
+    <View style={{ flex: 1 }}>
+      {isSignedIn ? (
+        <StockMarketTab />
+      ) : (
+        <ScrollView style={{ backgroundColor: COLORS.primary }}>
           <View style={{ justifyContent: "center", alignItems: "center" }}>
-          <Link href="/details" style={{ color: 'white', fontSize: 16, fontFamily: 'Arial', textAlign: 'center' }}>New user?</Link>
+            <Stack.Screen options={{ title: "Welcome to your bank", headerStyle: {backgroundColor: COLORS.red}, headerTitleStyle: { fontSize: 30, color: COLORS.white }}} />
+            <SignIn onSignIn={handleSignIn}/>
+            <View style={{ justifyContent: "center", alignItems: "center" }}>
+              <Link href="/details" style={{ color: 'white', fontSize: 16, fontFamily: 'Arial', textAlign: 'center' }}>New user?</Link>
+            </View>
           </View>
-        </TouchableOpacity>
-      </ScrollView>
+        </ScrollView>
+      )}
     </View>
   );
 }
