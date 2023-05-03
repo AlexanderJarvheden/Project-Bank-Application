b3d086974fe702d697f55dda0ce1a5a671e0260a
Aziz Ali <azizali@kth.se>
2023-05-03 22:07:06 +0200

    A new version of StockMarket class with some simple changes and comments

--- Interface/components/stockmarket/StockMarket.js
+++ Interface/components/stockmarket/StockMarket.js
@@ -1,43 +1,49 @@
 import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, ScrollView, TextInput } from 'react-native';
+import { View, StyleSheet, ScrollView, TextInput, Text, Button } from 'react-native';
 import { ListItem, Icon } from 'react-native-elements';
 import axios from 'axios';
 
 const StockMarketTab = () => {
-  const [popularStocks, setPopularStocks] = useState([]);
-  const [searchResults, setSearchResults] = useState([]);
-  const [searchText, setSearchText] = useState('');
-  const [balance, setBalance] = useState(10000);
-  const [portfolio, setPortfolio] = useState([]);
+const apiKey = 'YOUR_API_KEY';
+const popularStockSymbols = ['AAPL', 'GOOGL', 'MSFT', 'AMZN', 'TSLA', 'FB', 'BRK-B', 'JPM', 'JNJ', 'V'];
 
-  const apiKey = 'YOUR_API_KEY';
-  const popularStockSymbols = ['AAPL', 'GOOGL', 'MSFT', 'AMZN', 'TSLA', 'FB', 'BRK-B', 'JPM', 'JNJ', 'V'];
+const [popularStocks, setPopularStocks] = useState([]);
+const [searchResults, setSearchResults] = useState([]);
+const [searchText, setSearchText] = useState('');
+const [balance, setBalance] = useState(10000);
+const [portfolio, setPortfolio] = useState([]);
 
+  // Fetch popular stocks on component mount
   useEffect(() => {
     const fetchPopularStocks = async () => {
-      const promises = popularStockSymbols.map(symbol =>
-        axios.get(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${apiKey}`),
+      const promises = popularStockSymbols.map((symbol) =>
+        axios.get(
+          `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${apiKey}`
+        )
       );
 
       const results = await Promise.all(promises);
-      const stocks = results.map(res => res.data['Global Quote']);
+      const stocks = results.map((res) => res.data["Global Quote"]);
       setPopularStocks(stocks);
     };
 
     fetchPopularStocks();
   }, []);
 
+  // Search stocks based on input
   const searchStocks = async () => {
     const response = await axios.get(
-      `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${searchText}&apikey=${apiKey}`,
+      `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${searchText}&apikey=${apiKey}`
     );
     setSearchResults(response.data.bestMatches);
   };
 
+  // Buy stock functionality
   const buyStock = (symbol, price) => {
     // Implement your buy stock logic here
   };
 
+  // Sell stock functionality
   const sellStock = (symbol, price) => {
     // Implement your sell stock logic here
   };
@@ -50,94 +56,113 @@ const StockMarketTab = () => {
       </View>
       <TextInput
         style={styles.searchInput}
-        onChangeText={text => setSearchText(text)}
+        onChangeText={(text) => setSearchText(text)}
         onSubmitEditing={searchStocks}
         value={searchText}
         placeholder="Search for stocks"
       />
       <ScrollView>
-        {searchText === '' && (
+        {searchText === "" && (
           <View>
             <Text style={styles.sectionTitle}>Popular Stocks</Text>
             {popularStocks.map((stock, index) => (
               <ListItem key={index} bottomDivider>
                 <ListItem.Content>
-                  <ListItem.Title>{stock['01. symbol']}</ListItem.Title>
-                  <ListItem.Subtitle>${parseFloat(stock['05. price']).toFixed(2)}</ListItem.Subtitle>
+                  <ListItem.Title>{stock["01. symbol"]}</ListItem.Title>
+                  <ListItem.Subtitle>
+                    ${parseFloat(stock["05. price"]).toFixed(2)}
+                  </ListItem.Subtitle>
                 </ListItem.Content>
-                <Button title="Buy" onPress={() => buyStock(stock['01. symbol'], stock['05. price'])} />
-                <Button title="Sell" onPress={() => sellStock(stock['01. symbol'], stock['05. price'])} />
+                <Button
+                  title="Buy"
+                  onPress={() =>
+                    buyStock(stock["01. symbol"], stock["05. price"])
+                  }
+                />
+                <Button
+                  title="Sell"
+                  onPress={() =>
+                    sellStock(stock["01. symbol"], stock["05. price"])
+                  }
+                />
               </ListItem>
             ))}
           </View>
         )}
-        {searchText !== '' && (
+
+        {searchText !== "" && (
           <View>
             <Text style={styles.sectionTitle}>Search Results</Text>
-            {searchResults.map((stock,index) => (
-            <ListItem key={index} bottomDivider>
+            {searchResults.map((stock, index) => (
+              <ListItem key={index} bottomDivider>
                 <ListItem.Content>
-                    <ListItem.Title>{stock['1. symbol']}</ListItem.Title>
-                    <ListItem.Subtitle>{stock['2. name']}</ListItem.Subtitle>
-            </ListItem.Content>
-<Button
-title="Buy"
-onPress={() => buyStock(stock['1. symbol'], stock['2. name'])}
-/>
-<Button
-title="Sell"
-onPress={() => sellStock(stock['1. symbol'], stock['2. name'])}
-/>
-</ListItem>
-))}
-</View>
-)}
-<View>
-<Text style={styles.sectionTitle}>My Portfolio</Text>
-{portfolio.map((stock, index) => (
-<ListItem key={index} bottomDivider>
-<ListItem.Content>
-<ListItem.Title>{stock.symbol}</ListItem.Title>
-<ListItem.Subtitle>
-{stock.shares} shares @ ${parseFloat(stock.price).toFixed(2)}
-</ListItem.Subtitle>
-</ListItem.Content>
-<Button title="Sell" onPress={() => sellStock(stock.symbol, stock.price)} />
-</ListItem>
-))}
-</View>
-</ScrollView>
-</View>
-);
+                  <ListItem.Title>{stock["1. symbol"]}</ListItem.Title>
+                  <ListItem.Subtitle>{stock["2. name"]}</ListItem.Subtitle>
+                </ListItem.Content>
+                <Button
+                  title="Buy"
+                  onPress={() => buyStock(stock["1. symbol"], stock["2. name"])}
+                />
+                <Button
+                  title="Sell"
+                  onPress={() =>
+                    sellStock(stock["1. symbol"], stock["2. name"])
+                  }
+                />
+              </ListItem>
+            ))}
+          </View>
+        )}
+
+        <View>
+          <Text style={styles.sectionTitle}>My Portfolio</Text>
+          {portfolio.map((stock, index) => (
+            <ListItem key={index} bottomDivider>
+              <ListItem.Content>
+                <ListItem.Title>{stock.symbol}</ListItem.Title>
+                <ListItem.Subtitle>
+                  {stock.shares} shares @ ${parseFloat(stock.price).toFixed(2)}
+                </ListItem.Subtitle>
+              </ListItem.Content>
+              <Button
+                title="Sell"
+                onPress={() => sellStock(stock.symbol, stock.price)}
+              />
+            </ListItem>
+          ))}
+        </View>
+      </ScrollView>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-container: {
-flex: 1,
-paddingTop: 20,
-},
-balanceContainer: {
-flexDirection: 'row',
-alignItems: 'center',
-paddingHorizontal: 10,
-marginBottom: 10,
-},
-balanceText: {
-fontSize: 18,
-fontWeight: 'bold',
-marginLeft: 5,
-},
-searchInput: {
-height: 40,
-borderColor: 'gray',
-borderWidth: 1,
-paddingLeft: 8,
-marginBottom: 10,
-},
-sectionTitle: {
-fontSize: 20,
-fontWeight: 'bold',
-paddingLeft: 10,
-paddingBottom: 5,
-},
+  container: {
+    flex: 1,
+    paddingTop: 20,
+  },
+  balanceContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 10,
+    marginBottom: 10,
+  },
+  balanceText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginLeft: 5,
+  },
+  searchInput: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    paddingLeft: 8,
+    marginBottom: 10,
+  },
+  sectionTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    paddingLeft: 10,
+    paddingBottom: 5,
+  },
 });
