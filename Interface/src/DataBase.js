ee3b0a8154fbb1b610ff46c069ec5db93c599bdb
Joachim Olsson <joaco@kth.se>
2023-05-10 17:19:01 +0200

    no changes

--- Interface/src/DataBase.js
+++ Interface/src/DataBase.js
@@ -11,7 +11,6 @@ class Database {
       }
 
       await AsyncStorage.setItem(key, JSON.stringify(value));
-      console.log('User stored:', key, value); // Add this line
     } catch (error) {
       console.error('Error storing user data:', error);
     }
@@ -44,6 +43,8 @@ class Database {
       return null;
     }
   }
+
+
 
   static async removeUser(personalNumber) {
     try {
